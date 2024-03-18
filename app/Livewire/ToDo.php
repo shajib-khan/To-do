a677139a561da281cce02f01bbf650295460e76e
shajib-khan <shajibkhan946@gmail.com>
2024-03-18 15:05:32 +0600

    paginate add

--- app/Livewire/ToDo.php
+++ app/Livewire/ToDo.php
@@ -1,22 +1,28 @@
 <?php
 
 namespace App\Livewire;
-use App\Models\Todo as modeltodo;
+use App\Models\Todo as ModelTodo;
 use Livewire\Component;
 
 class ToDo extends Component
 {
-    public function todoadd()
+    public $todo_add;
+    public function addtodo()
     {
-        modeltodo::create([
-            'todo'=>'   This is new ToDo'
+        $this->validate([
+            "todo_add"=> "required"
         ]);
+        ModelTodo::create([
+            'todo'=> $this->todo_add
+
+        ]);
+        $this->todo_add="";
     }
 
 
     public function render()
     {
-        $alltodos = modeltodo::all();
+        $alltodos = ModelTodo::latest()->simplePaginate(3);
         return view('livewire.to-do', compact('alltodos'));
     }
 }
--- resources/views/livewire/to-do.blade.php
+++ resources/views/livewire/to-do.blade.php
@@ -1,18 +1,29 @@
 <div ="d-flex">
   <h3>This is ToDo</h3>
-   <button class="btn btn-primary" wire:click="todoadd">Add Todo</button>
+   <form wire:submit="addtodo">
+
+    <input type="text"wire:model.live="todo_add" placeholder="Add your to do">
+    @error('todo_add')
+    <div class="alert alert-danger">{{ $message }}</div>
+
+    @enderror
+    <button class="btn btn-primary">Add Todo</button>
+   </form>
    @forelse ($alltodos as $alltodo )
             <td>
                 <ul>
                     <li>
-                        <h4>{{ $alltodo->todo }}</h4>
-                        <p>Create {{ $alltodo->created_at }}</p>
-                        <p>Update {{ $alltodo->updated_at }}</p>
+
+                        <td><h4>{{ $alltodo->todo }}</h4></td>
+                        <td><p>Create {{ $alltodo->created_at }}</p></td>
+                        <td><p>Update {{ $alltodo->updated_at }}</p></td>
+                        <td><a class="btn btn-danger" wire:navigate="deletetodo" href="#">Delete</a></td>
                     </li>
                 </ul>
             </td>
-
    @empty
             <p>there is no todo</p>
                 @endforelse
+                {{ $alltodos->links() }}
+
 </div>
